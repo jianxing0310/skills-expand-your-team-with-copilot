db985aa76c269b242c8e7cff1e05bb8840c5e746
JianXing <king4647@gmail.com>
2026-02-12 13:15:17 +0800

    Add sample pytest test examples

---
+++ tests/test_example.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_add():
+    assert 1 + 1 == 2
+
+
+def test_subtract():
+    assert 1 - 1 == 0
+
+
+def test_multiply():
+    assert 2 * 3 == 6
+
+
+def test_divide():
+    assert 6 / 3 == 2
